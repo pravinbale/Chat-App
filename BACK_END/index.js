dec0081d0983de9bfb7c193184bfd9f69d85dd07
Pravin Bale <mr.xpravin2k17@gmail.com>
2024-07-09 23:55:16 +0530

    app completed

--- BACK_END/index.js
+++ BACK_END/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoute from "./routes/user.route.js";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -26,6 +27,16 @@ try {
 
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// code for deployment
+
+if (process.env.NODE_ENV === "production") {
+  const dirPath = path.resolve();
+  app.use(express.static("./FRONT_END/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirPath, "./FRONT_END/dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
